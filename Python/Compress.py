934c9bc8bd0f36de26e275739128b484b29a0b8f
BaggioAlessandro <alebaggio91@hotmail.it>
2014-06-11 12:30:18 +0200

    blocchi

--- Python/Compress.py
+++ Python/Compress.py
@@ -1,12 +1,12 @@
 import numpy as np
 import os
 
-def compress(n_file_input, factor, N_NEURONS, N_CAMP, n_file_output):
-    delay = np.empty((N_NEURONS,N_NEURONS,N_DELAYS), dtype=int)
-    for i in range(0, N_NEURONS):
+def compress(n_file_input, factor, n_neurons, N_CAMP, n_file_output):
+    delay = np.empty((n_neurons,n_neurons,N_CAMP), dtype=int)
+    for i in range(0, n_neurons):
         delay[i] = np.loadtxt(n_file_input+str(i)+".txt", dtype=int)
 
-    d1 = np.zeros ((N_NEURONS, N_NEURONS, np.floor(N_CAMP/factor)+1))
+    d1 = np.zeros ((n_neurons, n_neurons, np.floor(N_CAMP/factor)+1))
     for i in range(0, np.int64(np.floor(N_CAMP/factor))):   #check +1
         d1[:,:,i] = np.sum(delay[:,:,i*factor:(i+1)*factor], 2)
     
@@ -14,6 +14,6 @@ def compress(n_file_input, factor, N_NEURONS, N_CAMP, n_file_output):
         i = i+1
         d1[:,:,i]= np.sum(delay[:,:,(i)*f:], 2)
         
-    for i in range(0,N_NEURONS):
+    for i in range(0,n_neurons):
         np.savetxt(n_file_output+str(i)+".txt" ,delay[i,:,:], fmt = '%01d')
     
--- Python/edges_calc_v3.py
+++ Python/edges_calc_v3.py
@@ -3,44 +3,44 @@ import numpy as np
 
 def mySave_2D (my_matrix,fileName):
     file = open(path+ fileName +".txt", "w")
-    for i in range(0,N_NEURONS):
-        for j in range(0,N_NEURONS):
+    for i in range(0,n_neurons):
+        for j in range(0,n_neurons):
             file.write(str(my_matrix[i][j]) + " ")
         
         file.write("\n")    
     file.close
 
-def edges_cal(n_file, tresh_peak, weight_diff_fire, N_NEURONS):
+def edges_cal(n_file, tresh_peak, weight_diff_fire, n_neurons, n_delays):
     
     path = os.getcwd()
     path = path + "/Documents/GitHub/HPPS_NN/"
-    delay = np.empty((N_NEURONS,N_NEURONS,N_DELAYS), dtype=int)
-    for i in range(0, N_NEURONS):
+    delay = np.empty((n_neurons,n_neurons,n_delays), dtype=int)
+    for i in range(0, n_neurons):
         delay[i] = np.loadtxt(n_file+str(i)+".txt", dtype=int)
     
-    edges = np.zeros((N_NEURONS, N_NEURONS));
+    edges = np.zeros((n_neurons, n_neurons));
     
     d1_20 = delay[:,0:20,0:20]
 
     max = np.max(d1_20,1);
     
     
-    for i in range(0,N_NEURONS):
-        for j in range(0,N_NEURONS):
+    for i in range(0,n_neurons):
+        for j in range(0,n_neurons):
             if (d1_20[1,i,j] > max[i,j]*tresh_peak): #tresh_peak=0.85
                 edges[i,j] = 1;
             
-    for i in range(0,N_NEURONS):
-        for j in range(0,N_NEURONS):
+    for i in range(0,n_neurons):
+        for j in range(0,n_neurons):
             if (max[i,j] == d1_20[0,i,j] ):
                 edges[i,j] = 0;
             
-    for i in range(0,N_NEURONS):
-        for j in range(0,N_NEURONS):
+    for i in range(0,n_neurons):
+        for j in range(0,n_neurons):
             count=0;
             tot = 0;
             if (edges[i,j] == 1):
-                for h in range(0,N_NEURONS):
+                for h in range(0,n_neurons):
                     if (h != i and edges[h,j] == 1 ):
                         if ((d1_20[1,h,j]/2) > d1_20[1,i,j] * (d1_20[0,h,h] / d1_20[0,i,i]/weight_diff_fire)):  #weight_diff_fire = 0.5
                             count = count + 1;
@@ -50,7 +50,7 @@ def edges_cal(n_file, tresh_peak, weight_diff_fire, N_NEURONS):
                 if (count > 0):
                     edges[i,j] = 0;
                     
-    my_save2D(edges, "edges_delay_python")
+    mySave2D(edges, "edges_delay_python")
                 
             
         
