c7c7cef8041737908dbd5312fbfac2ff930cd1f7
lucaBuratti <luca.buratt@gmail.com>
2014-06-11 16:32:46 +0200

    correzione normalizazzione

--- Python/main.py
+++ Python/main.py
@@ -5,16 +5,22 @@ N_NEURONS = 10
 N_DELAYS = 100
 factor = 10
 
-#delay = np.empty((9,9,1000), dtype=int)
-#for i in range(0, 9):
- #   delay[i] = np.loadtxt(path + "delay_line_"+str(i)+".txt", dtype=int)
+#delay_hist = delayMatrics_v5("Input", 9, 3000000,1000,9)
 
-path1 = os.getcwd()
+
+path = os.getcwd()
+path1 = "C:/Users/Luca/Documents/GitHub/HPPS_NN/Delay_temp/Not_Compress/"
+delay = np.empty((9,9,1000), dtype=int)
+for i in range(0, 9):
+    delay[i] = np.loadtxt(path1 + "delay_line_"+str(i)+".txt", dtype=int)
+delay_n = normalHist(delay,9,1000)
+
+print(delay_n)
+
 path1 = path1 + "/Document/GitHub/HPPS_NN/Delay_temp/"
 
 path2 = os.getcwd()
 
-#delay_hist = delayMatrics_v5("01.txt", N_NEURONS, N_CAMP,100,10)
-compress("C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Not_Compress/delay_line_", factor, N_NEURONS, N_DELAYS, "C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Compress/delay_line_")
+#compress("C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Not_Compress/delay_line_", factor, N_NEURONS, N_DELAYS, "C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Compress/delay_line_")
 
-edges_cal("C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Compress/delay_line_", 0.85, 0.5, N_NEURONS, N_DELAYS)
+#edges_cal("C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Compress/delay_line_", 0.85, 0.5, N_NEURONS, N_DELAYS)
