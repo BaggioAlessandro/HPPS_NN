18c274b5af285b160796b6ea48bde301c71b01f9
lucaBuratti <luca.buratt@gmail.com>
2014-06-11 16:32:54 +0200

    Revert "correzione normalizazzione"
    This reverts commit c7c7cef8041737908dbd5312fbfac2ff930cd1f7.

--- Python/normalHist.py
+++ Python/normalHist.py
@@ -6,10 +6,8 @@ def normalHist(delay, n_neurons, n_delays):
     delay_n= np.zeros ((n_neurons,n_neurons,n_delays))
     for i in range(0,n_neurons):
         for j in range(0,n_neurons):
-            if sum[i][j] != 0:
-                delay_n[i][j] = [delay[i][j][p]/sum[i][j] for p in range(n_delays)]
-            else:
-                delay_n[i][j] = [0 for p in range(n_delays)]
-    return delay_n
+            delay_n[i][j] = [delay[i][j][p]/sum[i][j] for p in range(n_delays)]
+    
+    return delay_n;
     
 
