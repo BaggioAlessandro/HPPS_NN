5dc1429c4cab0474329be0408a63159f938284fe
BaggioAlessandro <alebaggio91@hotmail.it>
2014-06-12 13:19:02 +0200

    rete 10

--- Python/main.py
+++ Python/main.py
@@ -16,7 +16,7 @@ path1 = path1 + "/Document/GitHub/HPPS_NN/Delay_temp/"
 
 path2 = os.getcwd()
 
-#delay_hist = delayMatrics_v5("Input/10_old_model", N_NEURONS, N_CAMP,N_DELAYS,10)
+delay_hist = delayMatrics_v5("Input/10_old_model", N_NEURONS, N_CAMP,N_DELAYS,10,"Not_Compress")
 compress("C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Not_Compress/delay_line_", factor, N_NEURONS, N_DELAYS, media , "C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Compress/delay_line_")
 
 edges_cal("C:/Users/Ale/Documents/GitHub/HPPS_NN/Delay_temp/Compress/delay_line_", factor, 0.85, 0.5, N_NEURONS, N_DELAYS)
